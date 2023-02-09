17a5bde35a8ddd36ce1789ffcf81dc0fbdd3376d
boolayon <bryanhahuy@gmail.com>
2023-02-09 12:44:18 -0500

    Temporary confirm submit button press on login
    html now accesses login.js, and using defer, accesses it after load

--- js/login.js
+++ js/login.js
@@ -1,7 +1,8 @@
 const email = document.getElementById("email");
-const password = document.getElementById("password"):
+const password = document.getElementById("password");
 const submitAction = function (){
 	//do something with email.value and password.value
+	console.log("submitting email and password");
 
 	//if fail, display fail somewhere in red text
 }
