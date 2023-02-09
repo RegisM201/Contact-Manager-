cb25fd5aeaf147a12b095aed0c5085ab8bae18a8
boolayon <bryanhahuy@gmail.com>
2023-02-09 12:32:05 -0500

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,9 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password"):
+const submitAction = function (){
+	//do something with email.value and password.value
+
+	//if fail, display fail somewhere in red text
+}
+
+document.getElementById("submitButton").onclick = submitAction;
