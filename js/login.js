1a2921a3fe223128def96fe066aaf1deb092ea97
boolayon <bryanhahuy@gmail.com>
2023-02-09 18:38:11 -0500

    Update to css, js, login, signup
    made aesthetic changes

--- js/login.js
+++ js/login.js
@@ -3,8 +3,18 @@ const password = document.getElementById("password");
 const submitAction = function (){
 	//do something with email.value and password.value
 	console.log("submitting email and password");
-
+	if (!ValidateEmail(email.value)) console.log("rejected.");
 	//if fail, display fail somewhere in red text
+}
+
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
 }
 
 document.getElementById("submitButton").onclick = submitAction;
