965234702c4d77742fe043b328952329b3becb3c
Michael Regis <M_regis85@hotmail.com>
2023-02-09 19:08:20 -0500

    Update login.php

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -1,4 +1,5 @@
 <?php
+
 	header('Access-Control-Allow-Origin: *');
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
