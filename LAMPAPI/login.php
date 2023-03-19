bea083d15b3bcedc6cdea2f000283687e752e4a5
boolayon <bryanhahuy@gmail.com>
2023-03-19 14:00:11 -0400

    bug fix

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -49,7 +49,7 @@
 	}
 
 	function returnWithInfo($firstName, $lastName, $id) {
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . ',"error":""}';
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
 		sendResultInfoAsJson($retValue);
 	}
 	
