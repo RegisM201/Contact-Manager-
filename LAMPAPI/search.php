8c6674095f5c3356f3b6e985ecbedd87c50e4e61
boolayon <bryanhahuy@gmail.com>
2023-03-23 04:30:03 -0400

    Update search.php

--- LAMPAPI/search.php
+++ LAMPAPI/search.php
@@ -1,4 +1,7 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 	// Standard HTTP Request headers.
 	header('Access-Control-Allow-Origin: *');
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
